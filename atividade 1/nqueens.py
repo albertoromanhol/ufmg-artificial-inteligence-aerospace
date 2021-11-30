c27473ad353dc618e142c55ef1e290697dc937c1
albertoromanhol <albertoromanhol@gmail.com>
2021-11-29 21:21:24 -0300

    Tentativa 2 e melhorias

--- atividade 1/nqueens.py
+++ atividade 1/nqueens.py
@@ -1,9 +1,13 @@
 import random
+import numpy
 
 def find_best_fitness(max_fitness, population):
     generation = 0
-
-    while generation < 500 and not max_fitness in [fitness(queens) for queens in population]:
+    
+    max_generation = -13*len(population[0]) + 277
+    print('max_generation = {}'.format(max_generation))
+    
+    while generation < max_generation and not max_fitness in [fitness(queens) for queens in population]:
         generation += 1
         population = new_generation(population, fitness)
         print('generation = {}, fitness = {}'.format(generation, (max([fitness(n) for n in population]))))
@@ -11,39 +15,41 @@ def find_best_fitness(max_fitness, population):
     print('found a generation', generation)
     return population
 
-# VERIFICAR FUNCAO, MELHORAR, ALTERAR
 def fitness(chromosome):
-    horizontal_collisions = sum([chromosome.count(queen)-1 for queen in chromosome])/2
-
+    score = 0
     n = len(chromosome)
-    left_diagonal = [0] * 2*n
-    right_diagonal = [0] * 2*n
-    for i in range(n):
-        left_diagonal[i + chromosome[i] - 1] += 1
-        right_diagonal[len(chromosome) - i + chromosome[i] - 2] += 1
+    chromosome = numpy.array(chromosome) - 1
 
-    diagonal_collisions = 0
-    for i in range(2*n-1):
-        counter = 0
-        if left_diagonal[i] > 1:
-            counter += left_diagonal[i]-1
-        if right_diagonal[i] > 1:
-            counter += right_diagonal[i]-1
-        diagonal_collisions += counter / (n-abs(i-n+1))
-    
-    return int(max_collisions - (horizontal_collisions + diagonal_collisions))
+    for col in range(n): 
+        row = chromosome[col]  
+        
+        for other_col in range(n):
+            if other_col == col: # MESMA COLUNA 
+                continue
+            if chromosome[other_col] == row: # MESMA LINHA
+                continue
+            if other_col + chromosome[other_col] == col + row: # DIAGONAL SUPERIOR
+                continue
+            if other_col - chromosome[other_col] == col - row: # DIAGONAL INFERIOR
+                continue
+            # se nao atacar, pode ter score + 1, nao vai capturar
+            score += 1
+    return score/2
 
 def new_generation(population, fitness):
     new_population = []
     probabilities = [probability(n, fitness) for n in population]
+
     for _ in range(len(population)):
         parent1 = pick_parent(population, probabilities)
         parent2 = pick_parent(population, probabilities)
-        child = pick_child(parent1, parent2)
+        child = pick_children(parent1, parent2)
+        # escolhe a melhor dos dois filhosd = pick_childrens(parent1, parent2)
         # show_children(child)
         new_population.append(child)
 
         if fitness(child) == max_collisions: break
+
     return new_population
 
 def probability(chromosome, fitness):
@@ -62,46 +68,55 @@ def pick_parent(population, probabilities):
 # VERIFICAR SE PODE FAZER ISSO AQUI
 def selected_tournment(population):
     n = len(population)
-    return best_queen(population[random.randint(0, n-1):n])
+    return bigger_fitness(population[random.randint(0, n-1):n])
     
 # VERIFICAR ISSO AQUI
 def roulette_wheel(probabilities):
     total = sum(probabilities)
     pick = random.uniform(0, total)
     current = 0
+
     for i in range(len(probabilities)):
         current += probabilities[i]
         if current > pick:
             return i
 
-def pick_child(parent1, parent2):
-    mutation_probability = 0.05
+# certo, faz chross over com os parntes e retorna 2 filhos
+def pick_children(parent1, parent2):
 
-    child = crossover(parent1, parent2)
+    child1, child2 = crossover(parent1, parent2)
+    child1 = mutate(child1)
+    child2 = mutate(child2)
 
-    if (mutation_probability > random.random()):
-        child = mutate(child)
+    # escolhe a melhor dos dois filhos
+    child = bigger_fitness([child1, child2])
 
     return child
 
-# VERIFICAR ISSO AQUI
+# certo, pega os pais em uma posição aleatoria de 0 a n, divide e junta com o outro pai
 def crossover(x, y):
     n = len(x)
     c = random.randint(0, n - 1)
-    return x[0:c] + y[c:n]
+    return x[0:c] + y[c:n], y[0:c] + x[c:n]
 
-# VERIFICAR ISSO AQUI
+# certo, pega um chromossomo em posição aleatoria e define um valor aleatorio para ele 
 def mutate(chromosome):
-    n = len(chromosome)
-    position = random.randrange(0, n)
-    chromosome[position] = random.randrange(1, n + 1)
+    mutation_probability = 0.05
+
+    if (mutation_probability > random.random()):
+        n = len(chromosome)
+        position = random.randrange(0, n)
+        chromosome[position] = random.randrange(1, n + 1)
+
     return chromosome
 
+# certo, funcao para mostrar o chromossomo e o fitness desse
 def show_children(child):
     print('chromosome = {}, fitness = {}'
         .format(str(child), fitness(child)))
 
-def best_queen(population):
+# encontrando o best queen da populacao, maior fitness
+def bigger_fitness(population):
     best_fitness = 0
 
     for chrom in population:
@@ -111,6 +126,7 @@ def best_queen(population):
     
     return c
     
+# imprimir o board
 def print_board(board_queens, n):
     board = []
     for _ in range(n):
@@ -120,31 +136,33 @@ def print_board(board_queens, n):
     for row in board:
         print (" ".join(row))
 
+# gerar genotipo aleatorio para primeira popoulação
 def genotype_queens(n):
     return [ random.randint(1, n) for _ in range(n)]
     
+# programa mais, que trata do imput e chama as funcoes
 if __name__ == "__main__":
     n_queens = int(input("number of queens: "))
     max_collisions = (n_queens*(n_queens-1))/2
+    print('max fitness:', max_collisions)
 
     n_population = (2*n_queens)**2
     population = [genotype_queens(n_queens) for _ in range(n_population)]
 
     best_pouplation = find_best_fitness(max_collisions, population)
-    best_queen = best_queen(best_pouplation)
+    bigger_fitness = bigger_fitness(best_pouplation)
 
     print('-----------------------------------')
     print('n queen problem - genetic algorithm')
-    print('alberto romanhol moreira')
     print('n_queens:', n_queens)
     print('n_population:', n_population)
     print('max fitness:', max_collisions)
     print('-----------------------------------')
-    if fitness(best_queen) == max_collisions:
+    if fitness(bigger_fitness) == max_collisions:
         print('found a solution by fitness')
     else:
         print('no solution, stopped by generation limit')
-    print('found fitness:', fitness(best_queen))
-    print('best_queen:',best_queen)
-    print_board(best_queen, n_queens)
+    print('found fitness:', fitness(bigger_fitness))
+    print('best_queen:',bigger_fitness)
+    print_board(bigger_fitness, n_queens)
     print('-----------------------------------')
